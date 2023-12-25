c7ff363d4e1db89235b1e974acfc9124bd8cebe2
Tavbulatov <alikhantam26@gmail.com>
2023-12-25 17:10:46 +0300

    add weather api endpoints

---
+++ app/api/weather/api.rb
@@ -0,0 +1,63 @@
+<<~TEXT
+  GET  |  /health(.json)                  |  v1  |  Health.
+  GET  |  /weather/current(.json)         |  v1  |  Current temperature.
+  GET  |  /weather/historical(.json)      |  v1  |  Temperature in the last 24 hours.
+  GET  |  /weather/by_time(.json)         |  v1  |  By time.
+  GET  |  /weather/historical/max(.json)  |  v1  |  Max temperature in the last 24 hours.
+  GET  |  /weather/historical/min(.json)  |  v1  |  Min temperature in the last 24 hours.
+  GET  |  /weather/historical/avg(.json)  |  v1  |  Average temperature in the last 24 hours
+TEXT
+
+module Weather
+  class API < Grape::API
+    version 'v1', using: :header, vendor: 'twitter'
+    format :json
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      error!({ error: 'Not Found' }, 404)
+    end
+
+    desc 'Health.'
+    get :health do
+      status :ok
+    end
+
+    resources :weather do
+      desc 'Current temperature.'
+      get :current do
+        Forecast.current
+      end
+
+      desc 'Hourly temperature for the last 24 hours'
+      get :historical do
+        Forecast.historical
+      end
+
+      desc 'By time.'
+      params do
+        requires :timestamp, type: Integer, desc: 'Timestamp.'
+      end
+      get 'by_time/:timestamp' do
+        Forecast.by_time(params[:timestamp])['temperature']
+      end
+
+      resources :historical do
+        desc 'Max temperature in the last 24 hours'
+        get :max do
+          Forecast.max_t
+        end
+
+        desc 'Min temperature in the last 24 hours'
+        get :min do
+          Forecast.min_t
+        end
+
+        desc 'Average temperature in the last 24 hours'
+        get :avg do
+          Forecast.avg
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/request/weather_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe 'WeatherAPI', type: :request do
+  describe Weather::API do
+    let!(:forecasts) {create_list(:forecast, 10, type_weather: 'historical')}
+    let!(:forecast) {create(:forecast, epoch_time: Time.current)}
+
+    context 'GET /health' do
+      it 'returns status 200' do
+        get '/health'
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'GET /weather/current' do
+      it 'returns the current temperature' do
+        get "/weather/current"
+        expect(json).to eq forecast.temperature
+      end
+    end
+
+    context 'GET /weather/historical' do
+      it 'returns hourly temperature for the last 24 hours' do
+        get "/weather/historical"
+        expect(json).to eq Forecast.historical
+      end
+    end
+
+    context 'GET /weather/by_time' do
+      it 'return temperature by time.' do
+        get "/weather/by_time/#{Time.current.to_i}"
+        expect(json).to eq Forecast.by_time(Time.current.to_i).temperature
+      end
+
+      it 'epoch time exceeds current time' do
+        get "/weather/by_time/#{Time.current.to_i + 1.minutes.to_i}"
+        expect(json).to eq({"error"=>"Not Found"})
+      end
+
+      it 'epoch time is less than current time by more than 3 hours' do
+        get "/weather/by_time/#{Time.current.to_i - 4.hours.to_i}"
+        expect(json).to eq({"error"=>"Not Found"})
+      end
+
+      it 'return timestamp is invalid.' do
+        get "/weather/by_time/:timestamp"
+        expect(json).to eq({"error"=>"timestamp is invalid"})
+      end
+    end
+
+    context 'GET /weather/historical/max' do
+      it 'max temperature in the last 24 hours' do
+        get "/weather/historical/max"
+        expect(json).to eq Forecast.max_t
+      end
+    end
+
+    context 'GET /weather/historical/min' do
+      it 'min temperature in the last 24 hours' do
+        get "/weather/historical/min"
+        expect(json).to eq Forecast.min_t
+      end
+    end
+
+    context 'GET /weather/historical/avg' do
+      it 'average temperature in the last 24 hours' do
+        get "/weather/historical/avg"
+        expect(json.to_f).to eq Forecast.avg.to_f
+      end
+    end
+  end
+end
